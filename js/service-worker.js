0bc70a658b5b4b8cddf792e7e7fbd26eb5493c7e
Thomas Ipock <t.i@j7creations.com>
2026-03-29 01:42:53 -0500

    Center chapter content: .page max-width 1200px + margin auto inside content area

--- js/service-worker.js
+++ js/service-worker.js
@@ -5,7 +5,7 @@
    Dependencies: none
    ═══════════════════════════════════════════════════════════ */
 
-const CACHE_NAME = 'aoc-gdd-v46';
+const CACHE_NAME = 'aoc-gdd-v47';
 
 // Static assets to precache on install
 const PRECACHE_URLS = [
