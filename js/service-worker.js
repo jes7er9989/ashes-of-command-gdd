0ce6835847b6dcbfe8dc0b335eff5b79693b0629
Thomas Ipock <t.i@j7creations.com>
2026-03-29 01:45:23 -0500

    Center quote-block in chapters: margin auto + text-align center (was left-aligned override)

--- js/service-worker.js
+++ js/service-worker.js
@@ -5,7 +5,7 @@
    Dependencies: none
    ═══════════════════════════════════════════════════════════ */
 
-const CACHE_NAME = 'aoc-gdd-v47';
+const CACHE_NAME = 'aoc-gdd-v48';
 
 // Static assets to precache on install
 const PRECACHE_URLS = [
