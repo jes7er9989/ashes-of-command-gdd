4e6af990f27f670fe4baf42ab9a387a32092221c
Thomas Ipock <t.i@j7creations.com>
2026-03-30 01:05:43 -0500

    Center faction chapter headers: icon + title + tags centered
    All 7 faction chapters (ch5-ch11): header flex container changed
    to column layout, centered. Tags row also centered.
    SW v76

--- js/service-worker.js
+++ js/service-worker.js
@@ -5,7 +5,7 @@
    Dependencies: none
    ═══════════════════════════════════════════════════════════ */
 
-const CACHE_NAME = 'aoc-gdd-v75';
+const CACHE_NAME = 'aoc-gdd-v76';
 
 // Static assets to precache on install
 const PRECACHE_URLS = [
