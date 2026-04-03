03a9064a06208a3713c66b21122740ee3a9457cd
Thomas Ipock <t.i@j7creations.com>
2026-04-03 06:48:08 -0500

    CC-026: Ch12 zones expanded (resources/hazards/encounters/strategy) + star types table (hazards/zone freq)

--- js/service-worker.js
+++ js/service-worker.js
@@ -5,7 +5,7 @@
    Dependencies: none
    ═══════════════════════════════════════════════════════════ */
 
-const CACHE_NAME = 'aoc-gdd-v130';
+const CACHE_NAME = 'aoc-gdd-v131';
 
 // Static assets to precache on install
 const PRECACHE_URLS = [
