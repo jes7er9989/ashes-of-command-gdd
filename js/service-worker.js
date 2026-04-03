10457e96bf89ce89fe13b9b16eb1d9660ada0c32
Thomas Ipock <t.i@j7creations.com>
2026-04-03 10:40:09 -0500

    CC-038: Faction color consistency - Accord SVGs/text gold->green, Necro visual descriptions green->purple

--- js/service-worker.js
+++ js/service-worker.js
@@ -5,7 +5,7 @@
    Dependencies: none
    ═══════════════════════════════════════════════════════════ */
 
-const CACHE_NAME = 'aoc-gdd-v144';
+const CACHE_NAME = 'aoc-gdd-v145';
 
 // Static assets to precache on install
 const PRECACHE_URLS = [
