cad03ae7b76e6d9cf7e07cb656f1af47ecf343f3
Thomas Ipock <t.i@j7creations.com>
2026-04-03 09:06:06 -0500

    Ch11: sync visual degradation section with Ch12 lore changes (Bastion/Throne/Scar)

--- js/service-worker.js
+++ js/service-worker.js
@@ -5,7 +5,7 @@
    Dependencies: none
    ═══════════════════════════════════════════════════════════ */
 
-const CACHE_NAME = 'aoc-gdd-v136';
+const CACHE_NAME = 'aoc-gdd-v137';
 
 // Static assets to precache on install
 const PRECACHE_URLS = [
