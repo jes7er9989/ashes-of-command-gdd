27aff0df4ee3d5177a5ab79819dc8116f6541359
Thomas Ipock <t.i@j7creations.com>
2026-04-03 04:18:16 -0500

    Remove Volatile Death Chain Reactions from Five Differentiators

--- js/service-worker.js
+++ js/service-worker.js
@@ -5,7 +5,7 @@
    Dependencies: none
    ═══════════════════════════════════════════════════════════ */
 
-const CACHE_NAME = 'aoc-gdd-v116';
+const CACHE_NAME = 'aoc-gdd-v117';
 
 // Static assets to precache on install
 const PRECACHE_URLS = [
