58176e3389282e36057275904c7d81f657fd3179
Thomas Ipock <t.i@j7creations.com>
2026-04-03 17:56:15 -0500

    CC-051: Ice World + Ancient Ruins detailed cards in Ch13

--- js/service-worker.js
+++ js/service-worker.js
@@ -5,7 +5,7 @@
    Dependencies: none
    ═══════════════════════════════════════════════════════════ */
 
-const CACHE_NAME = 'aoc-gdd-v157';
+const CACHE_NAME = 'aoc-gdd-v158';
 
 // Static assets to precache on install
 const PRECACHE_URLS = [
