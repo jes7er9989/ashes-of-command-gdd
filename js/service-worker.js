eb1e628f3d2dd9403718ccda87a93959942b6646
Thomas Ipock <t.i@j7creations.com>
2026-04-03 06:51:27 -0500

    CC-027: Ch12 Warp Lanes rewritten - travel, blockades, wormholes, fog of war, exploration

--- js/service-worker.js
+++ js/service-worker.js
@@ -5,7 +5,7 @@
    Dependencies: none
    ═══════════════════════════════════════════════════════════ */
 
-const CACHE_NAME = 'aoc-gdd-v131';
+const CACHE_NAME = 'aoc-gdd-v132';
 
 // Static assets to precache on install
 const PRECACHE_URLS = [
