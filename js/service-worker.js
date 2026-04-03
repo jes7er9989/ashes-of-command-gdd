569a531f15c9770a790a685e7eeba7aa7837c0e0
Thomas Ipock <t.i@j7creations.com>
2026-04-03 11:23:48 -0500

    CC-043: Revenant Ch8 cleanup - Legion text, SVG green->purple tints, gradient ID renames

--- js/service-worker.js
+++ js/service-worker.js
@@ -5,7 +5,7 @@
    Dependencies: none
    ═══════════════════════════════════════════════════════════ */
 
-const CACHE_NAME = 'aoc-gdd-v149';
+const CACHE_NAME = 'aoc-gdd-v150';
 
 // Static assets to precache on install
 const PRECACHE_URLS = [
