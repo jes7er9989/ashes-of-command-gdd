4b6ddb30e31c21d8c43e091e307ff6037dc3dcb2
Thomas Ipock <t.i@j7creations.com>
2026-04-03 18:11:52 -0500

    CC-056: Scanning & Discovery mechanic section in Ch13

--- js/service-worker.js
+++ js/service-worker.js
@@ -5,7 +5,7 @@
    Dependencies: none
    ═══════════════════════════════════════════════════════════ */
 
-const CACHE_NAME = 'aoc-gdd-v162';
+const CACHE_NAME = 'aoc-gdd-v163';
 
 // Static assets to precache on install
 const PRECACHE_URLS = [
