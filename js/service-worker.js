a4fbca7cf706a327e4ae893c124c1ce1e6e7b496
Thomas Ipock <t.i@j7creations.com>
2026-04-03 09:58:29 -0500

    CC-033: Diegetic atmosphere - CRT overlay, hover brackets, radar scan, scrollbar, JetBrains Mono UI

--- js/service-worker.js
+++ js/service-worker.js
@@ -5,7 +5,7 @@
    Dependencies: none
    ═══════════════════════════════════════════════════════════ */
 
-const CACHE_NAME = 'aoc-gdd-v139';
+const CACHE_NAME = 'aoc-gdd-v140';
 
 // Static assets to precache on install
 const PRECACHE_URLS = [
