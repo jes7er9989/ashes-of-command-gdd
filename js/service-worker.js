7be36b7555e9b4889bbca8285a8221192148eb26
Thomas Ipock <t.i@j7creations.com>
2026-04-03 04:53:10 -0500

    Fix: Nav.go does not exist - replace all 47 calls with Nav.route (the actual method)

--- js/service-worker.js
+++ js/service-worker.js
@@ -5,7 +5,7 @@
    Dependencies: none
    ═══════════════════════════════════════════════════════════ */
 
-const CACHE_NAME = 'aoc-gdd-v124';
+const CACHE_NAME = 'aoc-gdd-v125';
 
 // Static assets to precache on install
 const PRECACHE_URLS = [
