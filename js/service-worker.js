56a2eaa0a3d819a9668a26aed1bc4ccef95a6b9a
Thomas Ipock <t.i@j7creations.com>
2026-04-03 18:14:26 -0500

    CC-057: Revenant Sleeper Cache system section in Ch13

--- js/service-worker.js
+++ js/service-worker.js
@@ -5,7 +5,7 @@
    Dependencies: none
    ═══════════════════════════════════════════════════════════ */
 
-const CACHE_NAME = 'aoc-gdd-v163';
+const CACHE_NAME = 'aoc-gdd-v164';
 
 // Static assets to precache on install
 const PRECACHE_URLS = [
