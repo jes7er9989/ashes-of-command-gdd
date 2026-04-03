1c615d53ec1ee6c1922aa52079e22d0543436695
Thomas Ipock <t.i@j7creations.com>
2026-04-03 17:54:03 -0500

    CC-050: Jungle + Desert detailed cards in Ch13

--- js/service-worker.js
+++ js/service-worker.js
@@ -5,7 +5,7 @@
    Dependencies: none
    ═══════════════════════════════════════════════════════════ */
 
-const CACHE_NAME = 'aoc-gdd-v156';
+const CACHE_NAME = 'aoc-gdd-v157';
 
 // Static assets to precache on install
 const PRECACHE_URLS = [
