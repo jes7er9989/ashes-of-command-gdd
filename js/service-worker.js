1f5cb47cb2aae51054175932867f7d632a16f6a1
Thomas Ipock <t.i@j7creations.com>
2026-04-04 05:25:12 -0500

    CC-074: Ch19 ground battle flow, generals, ROE, variable win conditions

--- js/service-worker.js
+++ js/service-worker.js
@@ -5,7 +5,7 @@
    Dependencies: none
    ═══════════════════════════════════════════════════════════ */
 
-const CACHE_NAME = 'aoc-gdd-v180';
+const CACHE_NAME = 'aoc-gdd-v181';
 
 // Static assets to precache on install
 const PRECACHE_URLS = [
