14a8242aaa76fa75578e5c0f1c4c97b1f09afe38
Thomas Ipock <t.i@j7creations.com>
2026-04-03 19:05:51 -0500

    CC-059: Moon Generation & Typing rules section in Ch13

--- js/service-worker.js
+++ js/service-worker.js
@@ -5,7 +5,7 @@
    Dependencies: none
    ═══════════════════════════════════════════════════════════ */
 
-const CACHE_NAME = 'aoc-gdd-v165';
+const CACHE_NAME = 'aoc-gdd-v166';
 
 // Static assets to precache on install
 const PRECACHE_URLS = [
