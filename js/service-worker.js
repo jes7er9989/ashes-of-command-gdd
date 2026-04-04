35e8a84801f4666d580e46d117634d1ad21b57bf
Thomas Ipock <t.i@j7creations.com>
2026-04-03 20:05:22 -0500

    CC-068: Rewritten megastructure SVGs - Orbital Station quality benchmark, detailed metallic rendering

--- js/service-worker.js
+++ js/service-worker.js
@@ -5,7 +5,7 @@
    Dependencies: none
    ═══════════════════════════════════════════════════════════ */
 
-const CACHE_NAME = 'aoc-gdd-v174';
+const CACHE_NAME = 'aoc-gdd-v175';
 
 // Static assets to precache on install
 const PRECACHE_URLS = [
