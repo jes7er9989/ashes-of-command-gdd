72538fa0888b977226620258d4b52191267dfaba
Thomas Ipock <t.i@j7creations.com>
2026-04-03 19:10:09 -0500

    CC-061: Ch8 Revenant sleeper/excavation cross-references to Ch13

--- js/service-worker.js
+++ js/service-worker.js
@@ -5,7 +5,7 @@
    Dependencies: none
    ═══════════════════════════════════════════════════════════ */
 
-const CACHE_NAME = 'aoc-gdd-v167';
+const CACHE_NAME = 'aoc-gdd-v168';
 
 // Static assets to precache on install
 const PRECACHE_URLS = [
