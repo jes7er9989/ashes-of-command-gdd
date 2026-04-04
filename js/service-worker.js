a7b1ca4f66cccd77a60183624ab1ea5c44d5803d
Thomas Ipock <t.i@j7creations.com>
2026-04-04 11:34:53 -0500

    CC-084: Vorax Ch10 opening rewrite + SW v189 - Eldritch deep-time horror (HUNGRY longer than Milky Way has had stars) - Physical identity front-loaded (void-black armor, translucent skin, bioluminescent organs) - Anglerfish metaphor made literal (The Vorax ARE the lures) - Para 3 philosophical mirror preserved

--- js/service-worker.js
+++ js/service-worker.js
@@ -5,7 +5,7 @@
    Dependencies: none
    ═══════════════════════════════════════════════════════════ */
 
-const CACHE_NAME = 'aoc-gdd-v188';
+const CACHE_NAME = 'aoc-gdd-v189';
 
 // Static assets to precache on install
 const PRECACHE_URLS = [
