fb00c5f7e87f17b11edd4a1a462bee95b88e2475
Thomas Ipock <t.i@j7creations.com>
2026-04-03 19:11:47 -0500

    CC-062: Ch14 + Ch15 cross-references to Ch13 planet systems

--- js/service-worker.js
+++ js/service-worker.js
@@ -5,7 +5,7 @@
    Dependencies: none
    ═══════════════════════════════════════════════════════════ */
 
-const CACHE_NAME = 'aoc-gdd-v168';
+const CACHE_NAME = 'aoc-gdd-v169';
 
 // Static assets to precache on install
 const PRECACHE_URLS = [
