ea8eda2e4fc19170c1ef895502be6c694c42b5f4
Thomas Ipock <t.i@j7creations.com>
2026-04-06 09:44:12 -0500

    Cache-bust planet-renderer.js to force CDN refresh (v218)

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@
    Dependencies: none
    ═══════════════════════════════════════════════════════════ */
 
-const CACHE_NAME = 'aoc-gdd-v217';
+const CACHE_NAME = 'aoc-gdd-v218';
 
 // Static assets to precache on install
 const PRECACHE_URLS = [
