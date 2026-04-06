8a711a26b1024929e32938092e7575804de75675
Thomas Ipock <t.i@j7creations.com>
2026-04-06 11:28:02 -0500

    Worldship: instant death on contact, never reached the edge (v231)

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@
    Dependencies: none
    ═══════════════════════════════════════════════════════════ */
 
-const CACHE_NAME = 'aoc-gdd-v230';
+const CACHE_NAME = 'aoc-gdd-v231';
 
 // Static assets to precache on install
 const PRECACHE_URLS = [
