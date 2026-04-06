e867496fd93efab20a88646ac53b19efa03bf8fa
Thomas Ipock <t.i@j7creations.com>
2026-04-06 12:56:48 -0500

    Worldship: secondary points doubled again (0.8 amplitude) (v247)

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@
    Dependencies: none
    ═══════════════════════════════════════════════════════════ */
 
-const CACHE_NAME = 'aoc-gdd-v246';
+const CACHE_NAME = 'aoc-gdd-v247';
 
 // Static assets to precache on install
 const PRECACHE_URLS = [
